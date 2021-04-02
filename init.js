f0511594a0ef3e1a0219642e3fbabb9cd4716ef9
TakanashiGin <takanashi.gin.jul@gmail.com>
2021-04-02 21:36:06 +0900

    v1.1 処理を簡略化

--- init.js
+++ init.js
@@ -7,29 +7,16 @@ tyrano.plugin.kag.stat.change_layer_order = {
 // 専用のメソッドを定義
 $.change_layer_order = {
 
-    // 現在表示されているHTML要素を返す
-    getOriginElements: function(content){
-        let origin_elements;
-        if (content == 'free') {
-            const elem = $('#root_layer_game').children();
-            origin_elements = elem.filter(layer => $(elem[layer]).attr('class').indexOf('base') === -1);
-        } else if (content == 'message') {
-            const elem = $('#root_layer_system').children();
-            origin_elements = elem.filter(layer => $(elem[layer]).attr('class').indexOf('message') !== -1);
-        }
-        return origin_elements;
-    },
-
     // 実際にHTML要素をソートする
-    sort: function(modified_elements_class){
-        const j_after = modified_elements_class.map(cls => $(`.${cls}`));
+    sort: function(modified_elements){
+        const j_after = modified_elements.map(cls => $(`.${cls}`));
         const j_top = j_after.shift();
         j_top.after(...j_after);
     },
 
     // 配列中に重複している要素の確認
     existsSameValue: function(a){
-        var s = new Set(a);
+        const s = new Set(a);
         return s.size != a.length;
     }
 
@@ -50,11 +37,14 @@ tyrano.plugin.kag.tag.change_layer_order = {
         const content = pm.content;
         const order = pm.order.split(',');
 
+        // config.tjsの値
+        const config_layer_num = Number(content == 'free'? this.kag.config.numCharacterLayers : this.kag.config.numMessageLayers);
+
         // order属性が適切に指定されているか
-        if ((content == 'free'? Number(this.kag.config.numCharacterLayers) : Number(this.kag.config.numMessageLayers)) < order.length) {
+        if (config_layer_num < order.length) {
             console.error('[Change Layer Order] order属性にしているレイヤ数がティラノに登録されているレイヤ数を超えています');
             return false;
-        } else if ((content == 'free'? Number(this.kag.config.numCharacterLayers) : Number(this.kag.config.numMessageLayers)) > order.length) {
+        } else if (config_layer_num > order.length) {
             console.error('[Change Layer Order] order属性にしているレイヤ数がティラノに登録されているレイヤ数より少ないです');
             return false;
         }
@@ -63,34 +53,25 @@ tyrano.plugin.kag.tag.change_layer_order = {
             return false;
         }
 
-        // 順番を変更する元の要素郡を取得
-        const origin_elements = $.change_layer_order.getOriginElements(content);
-        //for (let e of origin_elements) console.log($(e).attr('class'));
-
-        // 要素郡の順番を変更
+        // 並び替える要素郡のClass
         const modified_elements = [];
-        order.forEach(num => {
-            let fore, back;
-            for (let e of origin_elements) {
-                const cl = $(e).attr('class');
-                if (cl.indexOf(`${num}_fore`) !== -1) fore = e;
-                else if (cl.indexOf(`${num}_back`) !== -1) back = e;
+        for (let i=0; i<order.length; i++) {
+            let fore = `${order[i]}_fore`;
+            let back = `${order[i]}_back`;
+            if (content == "message") {
+                fore = "message" + fore;
+                back = "message" + back;
             }
             modified_elements.push(fore);
             modified_elements.push(back);
-        });
-        //console.log(modified_elements);
-
-        // 順番変更済の要素郡からClass名を抽出
-        const modified_elements_class = modified_elements.map(e => $(e).attr('class').split(' ')).map(cl => cl.filter(v => (v.indexOf('_fore') !== -1 || v.indexOf('_back') !== -1) && v.indexOf('layer') === -1).shift());
-        //console.log(modified_elements_class);
+        }
 
         // 順番変更済の要素郡のClass名を stat に保存（make.ks用）
-        this.kag.stat.change_layer_order[content] = modified_elements_class.map(v => v);
+        this.kag.stat.change_layer_order[content] = modified_elements.map(v => v);
         //console.log(this.kag.stat.change_layer_order);
 
         // HTML要素を実際に操作
-        $.change_layer_order.sort(modified_elements_class);
+        $.change_layer_order.sort(modified_elements);
 
         this.kag.ftag.nextOrder();
 
